ca8d1e25bc44bf12478a0066a748e6b08602e8dc
Yapei Yu <fishyuyapei@163.com>
2022-06-14 18:56:26 +0100

    load economic & demographic data

--- R SCRIPT/00_Get origin data.R
+++ R SCRIPT/00_Get origin data.R
@@ -1,10 +1,6 @@
 # 00 Aim to get the the origin data
 
 
-# ################################################################# #
-#### GET Boundary                                                ####
-# ################################################################# #
-
 ### Clear memory
 rm(list = ls())
 
@@ -15,7 +11,6 @@ library(osmdata)
 library(dplyr)
 library(dismo)
 library(deldir)
-library(maptools)
 
 tmap_mode("view")
 
@@ -54,50 +49,59 @@ traffic1_2018 <- st_transform(traffic1_2018,27700)
 traffic2_2018 <- traffic2[traffic2$year==2018,] #filter the count obtained in 2018
 traffic2_2018 <- traffic2_2018[,c("road_name","road_type","all_motor_vehicles")] 
 
-names(traffic2_2018)<-c("road_name","road_type","aadt","geometry")
-traffic2_2018 <- st_transform(traffic2_2018,27700)
+names(traffic2_2018) <- c("road_name","road_type","aadt","geometry")
+traffic2_2018 <- st_transform (traffic2_2018,27700)
 
-traffic_2018 <- rbind(traffic1_2018,traffic2_2018)
+traffic_2018 <- rbind (traffic1_2018,traffic2_2018)
 
 
+# ################################################################# #
+#### GET Boundary                                                ####
+# ################################################################# #
 
 ### Make Bounds and save the traffic count within the bound
 bound_buf <- st_convex_hull(st_union(traffic1_2018)) # Make Polygon Around Traffic Data
 bound_buf <- st_buffer(bound_buf, 1000) # Buffer Polygon by 1km
 
 qtm(bound_buf)+qtm(traffic_2018,dots.col = "aadt")
+
 traffic_2018 <- traffic_2018[bound_buf,]
 
 st_write(bound_buf, "Data/00_bound_buf.gpkg", delete_dsn = TRUE)
 saveRDS(traffic_2018,"Data/00_traffic_camb_2018.RDS")
+
+
 
 # ################################################################# #
 #### GET Economic and Demographic Data                           ####
 # ################################################################# #
 
 ### Load LSOA boundary
-# Cambridge_lsoa <- read_sf(dsn = "Data/Cambridge_lsoa_2011.shp")
-# East Cambridgeshire_lsoa <- st_read("Data/East Cambridgeshire_lsoa_2011.shp")
-# Fenland_lsoa <- st_read("Data/Fenland_lsoa_2011.shp")
-# Hantingtonshire_lsoa <- readShapeSpatial("Data/Hantingtonshire_lsoa_2011.shp")
-# Peterborough_lsoa <- readShapeSpatial("Data/Peterborough_lsoa_2011.shp")
-# South Cambridgeshire_lsoa <- readShapeSpatial("Data/South Cambridgeshire_lsoa_2011.shp")
+lsoa <- read_sf("E:/R_language/Dissertation/Data/infuse_lsoa_lyr_2011_clipped/infuse_lsoa_lyr_2011_clipped.shp")
+lsoa <- lsoa[substr(lsoa$geo_code,1,1) == "E",]
+st_write(lsoa, "Data/00_LSOA_England.gpkg",delete_dsn = TRUE)
 
+lsoa_Camb <- lsoa[bound_buf,]
+qtm(bound_buf) + qtm(lsoa_Camb, fill = NULL)
 
-lsoa <- readRDS("Data/LSOA_Cambridgeshire.Rds")
+st_write(lsoa_Camb, "Data/00_LSOA_Cambridgeshire.gpkg")
+# lsoa <- readRDS("Data/LSOA_Cambridgeshire.Rds")
 
 
 ### Load population and car ownership original data
 pop_carown <- read.csv (file="Data/PBCC_LSOA_data.csv",header = TRUE)
-# distri <- c("Cambridge", "East Cambridgeshire", "Fenland", "Hantingtonshire", 
-#             "Peterborough", "South Cambridgeshire")
-# pop_carown <- pop_carown[pop_carown$LAD17NM == distri,]
-pop_carown <- pop_carown[pop_carown$LSOA11 %in% lsoa$geo_code, ]
+pop_carown <- pop_carown[pop_carown$LSOA11 %in% lsoa_Camb$geo_code, ]
 pop_carown <- pop_carown[,c("LSOA11","cars_percap_2018","pop_2018", "T2W_CarOrVan","km_CarOrVan","T2S_car")]
 
-lsoa <- left_join(lsoa, pop_carown, by = c("geo_code" = "LSOA11"))
-tm_shape(lsoa) +
+demo_eco <- left_join(lsoa_Camb, pop_carown, by = c("geo_code" = "LSOA11"))
+tm_shape(demo_eco) +
   tm_fill("cars_percap_2018", breaks = c(0,0.2,0.4,0.6,1,2,70))
 
 ### Load employment original data
-employ <- read.csv (file = "Data/employment.csv", header = TRUE)
+employ <- read.csv (file = "Data/employment_Camb_2018.csv", header = TRUE)
+# employ_empt <- demo_eco[!demo_eco$geo_label %in% employ$geo_name,]
+demo_eco <- left_join(demo_eco, employ, by = c("geo_code" = "mnemonic"))
+tm_shape(demo_eco) +
+  tm_fill("Employment", breaks = c(0,200,400,600,1000,2000,5000))                    
+
+saveRDS(demo_eco, "Data/00_demo_eco.RDS")
