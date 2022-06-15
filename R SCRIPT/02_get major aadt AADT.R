f92a03afbaf31715a0f57dccc0ed04b380ba46bd
Yapei Yu <fishyuyapei@163.com>
2022-06-15 01:33:15 +0100

    Update 02_get major aadt AADT.R

--- R SCRIPT/02_get major aadt AADT.R
+++ R SCRIPT/02_get major aadt AADT.R
@@ -31,7 +31,7 @@ points_junc <- st_read("Data/01_junctions.gpkg")
 
 
 # ################################################################# #
-#### GET AADT COUNTS                                             ####
+#### Allocate road name to the network                          ####
 # ################################################################# #
 major_ref <- c("motorway","motorway_link","primary",
                "primary_link","trunk","trunk_link")
@@ -69,16 +69,14 @@ for(k in 1:2){
 }
 
 
-
 ### manually check the ref and fix the unreasonable ones
 qtm(lines_major, lines.col = "ref", lines.lwd = 2)
 lines_major$ref[lines_major$osm_id == "148927689"] <-  "A1" 
-              # Based on the map to find the lost road and fix it
-
-qtm(bound)+qtm(lines_major, lines.col = "ref", 
-               lines.lwd = 2) + qtm(traffic_2018,dots.col = "aadt")
+              # Based on the map to find the lost / wrong road and fix it
+lines_major$ref[lines_major$osm_id == "534093170"] <-  "A1307" 
 
 rm(nn,lines_major_cents)
+
 
 # ############################################ #
 ####   Assign Traffic Counts to the roads   ####
@@ -90,8 +88,6 @@ get.aadt.major <- function(e){
   traffic.sub <- traffic_2018.major[traffic_2018.major$road_name == roadnames[e],]
   traffic.sub <- traffic.sub[!duplicated(traffic.sub$geometry),]
   lines.sub <- lines.nona[lines.nona$ref == roadnames[e],]
-  
-  
 
   ### need at least 2 points to make voronoi polygons, a cell consisting of
   ### all points of the plane closer to a certain seed than to any other
@@ -100,24 +96,21 @@ get.aadt.major <- function(e){
     voronoi <- st_voronoi(st_combine(traffic.sub))
     voronoi <- st_collection_extract(voronoi)
     voronoi <- st_as_sf(voronoi)
+    voronoi <- st_join(voronoi, traffic.sub) #based on the geom
 
   }else{
     ### Make a 500 buffer around the point
-    voronoi <- st_buffer(traffic.sub, 500)
+    voronoi <- st_buffer(traffic.sub, 5000)
   }
-   
-  
-  voronoi <- st_join(voronoi, traffic.sub) #based on the geom
+qtm(voronoi) + qtm(lines.sub, lines.col = "aadt") + qtm(traffic.sub,dots.col = "aadt")
+ 
 
   ### Find Intersections of roads with voronoi polygons
   inter <- st_intersects(lines.sub,voronoi)
   ### Get aadt and ncycle values 1:nrow(lines.sub)
-  lines.sub$aadt <- sapply(1:nrow(lines.sub),function(x)
+  lines.sub$aadt <-sapply(1:nrow(lines.sub),function(x)
     {as.numeric(round(mean(voronoi$aadt[inter[[x]]])),0)})
 
-  qtm(voronoi, fill = NULL) + qtm(traffic.sub) +
-    tm_shape(lines.sub) +
-    tm_lines(col = "aadt", style = "jenks", palette = "Spectral", lwd = 3)
   ### Remove Unneeded Data
   lines.sub <- as.data.frame(lines.sub)
   lines.sub <- lines.sub[,c("osm_id","aadt")]
@@ -125,7 +118,7 @@ get.aadt.major <- function(e){
   return(lines.sub)
 }
 
-### find the road_name of AADT different with the line$ref and fix them
+### find the road_name of AADT different from the line$ref and fix them
 roadnames <- unique(traffic_2018$road_name)
 roadnames.sub <- roadnames[!roadnames %in% lines$ref] 
 traffic_2018$road_name[traffic_2018$road_name == "A14(M)"] <-  "A14"
@@ -148,28 +141,36 @@ res.major <- lapply(1:length(roadnames),get.aadt.major)
              #apply a function over a list or vector
 res.major <- do.call("rbind",res.major) #rbind the data.frame
 res.major <- res.major[!is.na(res.major$osm_id),]
-rm(lines.nona,roadnames)
 
 
 ### remove any duplicates
 res.major <- res.major[!duplicated(res.major$osm_id),]
 res.major$aadt <- as.numeric(res.major$aadt)
 
+
 ### Join onto the original lines data
 lines_major <- left_join(lines_major,res.major, by = c("osm_id" = "osm_id"))
-rm(res.major)
+rm(lines.nona, roadnames, res.major)
 
-### get here
 qtm(bound) + qtm(lines_major[lines_major$highway %in% c("motorway","motorway_link","primary","primary_link",
                            "trunk","trunk_link"),]
     , lines.lwd = 3, lines.col = "aadt") + qtm(traffic_2018.major, dots.col="aadt")
 
+
 ### Manually fix the road sections with NA aadt value
-lines_major.sub <- subset (lines_major, is.na(lines_major$aadt))
+lines_major <- lines_major[lines_major$ref!="A1122",]
+                       # The A1122 whose aadt is NA isn't the road in the study case
 
+aadt_na <- lines_major[is.na(lines_major$aadt),]
+lines_major <- lines_major[!is.na(lines_major$aadt),]
+aadt_na$aadt <- 12593 #use the aadt from the nearest road section
+lines_major <- rbind(lines_major,aadt_na)
 
+summary(is.na(lines_major$aadt))
 
-
+# ################################################################# #
+####   Allocate the aadt to the major and minor road junctions   ####
+# ################################################################# #
 
 ### Find Junctions between minor and major roads 
 minor_int <- st_intersects(points_junc, lines_minor)
@@ -188,16 +189,16 @@ qtm(junc_majmi, dots.col = "aadt")
 
 
 ### Allocate the traffic counts to the minor roads
-traffic_2018.minor <- traffic_2018.minor[,"aadt"]
-lines_minor <- st_join(lines_minor, traffic_2018.minor,st_is_within_distance, dist = 7)
+#traffic_2018.minor <- traffic_2018.minor[,"aadt"]
+#lines_minor <- st_join(lines_minor, traffic_2018.minor,st_is_within_distance, dist = 7)
                                   # Buffer as traffic points are away from roads
-summary(is.na(lines_minor$aadt))  # Check if the traffic counts number is right
-qtm(lines_minor, lines.lwd = 3, lines.col = "aadt") 
-       + qtm (traffic_2018.minor, dots.col ="aadt")
+#summary(is.na(lines_minor$aadt))  # Check if the traffic counts number is right
+#qtm(lines_minor, lines.lwd = 3, lines.col = "aadt") 
+#       + qtm (traffic_2018.minor, dots.col ="aadt")
 
-lines <- rbind(lines_major, lines_minor)
+#lines <- rbind(lines_major, lines_minor)
 
 
 st_write(lines_minor, "Data/02_lines_minor.gpkg", delete_dsn = TRUE)
-st_write(lines, "Data/02_lines.gpkg", delete_dsn = TRUE)
+st_write(lines_major, "Data/02_lines_major.gpkg", delete_dsn = TRUE)
 st_write(junc_majmi, "Data/02_junctions.gpkg", delete_dsn = TRUE)
