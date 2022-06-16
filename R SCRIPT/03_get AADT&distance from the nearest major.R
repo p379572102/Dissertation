b3a3c25c5e3232f0d6fdcbaf7bd6638b17a55279
Yapei Yu <fishyuyapei@163.com>
2022-06-16 17:50:03 +0100

    180 minor road cannot get major aadt
    After applying the dodgr_times functions, there are 180 minor road cannot get the major aadt. Some of them do intersect with the major road, while others don't. The codes showing the results are line 68-70.

--- R SCRIPT/03_get AADT&distance from the nearest major.R
+++ R SCRIPT/03_get AADT&distance from the nearest major.R
@@ -19,8 +19,11 @@ tmap_mode("view")
 
 ### Load Data ---------------------------------------------------------------
 
+bound<-st_read("Data/00_bound_buf.gpkg")
 lines_minor <- st_read("Data/02_lines_minor.gpkg")
-lines <- st_read("Data/02_lines.gpkg")
+lines_major <- st_read("Data/02_lines_major.gpkg")
+#lines <- st_read("Data/02_lines.gpkg")
+lines <- st_read("Data/01_network.gpkg")
 junc_majmi <- st_read("Data/02_junctions.gpkg")
 
 # ################################################################# #
@@ -31,71 +34,91 @@ junc_majmi <- st_read("Data/02_junctions.gpkg")
 lines_minor <- st_transform(lines_minor, 4326)
 junc_majmi <- st_transform(junc_majmi, 4326)
 
+
 ### Get mid-point of minor roads, i.e. centroid on the line
-minor_cent <- as.data.frame(st_coordinates(lines_minor))
-minor_cent <- group_by(minor_cent, L1) %>% # Based on L1 to group the minor_cent
+point_minor <- as.data.frame(st_coordinates(lines_minor))
+point_minor <- group_by(point_minor, L1) %>% # Based on L1 to group the junc_minor
   summarise(X = nth(X, n()/2),
-            Y = nth(Y, n()/2)) # use the underlying median item as the group representative
+            Y = nth(Y, n()/2)) 
+                                  # use the n/2 item as the group represent
 
 ### Make dodgr graph of minor roads
 graph <- weight_streetnet(lines_minor, wt_profile = "motorcar")
+                             # use motorcar mode to weight the minor road
 graph_ids <- graph[,c("from_id","from_lon","from_lat")]
-graph_ids <- unique(graph_ids)
+                 # focus on the certain start point rather than the end points
+graph_ids <- unique(graph_ids)  
 
 junc_majmi <- cbind(junc_majmi, st_coordinates(junc_majmi))
 junc_majmi <- left_join(junc_majmi, graph_ids, by = c("X" = "from_lon",
                                                       "Y" = "from_lat"))
-minor_cent <- left_join(minor_cent, graph_ids, by = c("X" = "from_lon",
-                                                      "Y" = "from_lat"))
+                            # link the start point which is the majmi junction to the data.frame
+point_minor <- left_join(point_minor, graph_ids, by = c("X" = "from_lon",
+                                                        "Y" = "from_lat"))
+                            # link the start point which is the minor junction to the data.frame
 
-# For each minor road centroid, find the nearest (in time) junction  --------
+
+
+# For each minor road , find the nearest (in time) junction  --------
+
 dists <- dodgr_times(graph,
                      from = junc_majmi$from_id,
-                     to = minor_cent$from_id,
+                     to = point_minor$from_id,
                      shortest = FALSE)
+notna <- colSums(!is.na(dists))
+notna <- as.data.frame(notna)
+summary(notna$notna==0)
+         #The result shows 180 minor roads don't acquire any value
 
 saveRDS(dists,"Data/03_dists_matrix.Rds")
 
-nearst_junction <- list()
-distance_junction <- list()
+nearest_junc <- list()
+dist_junc <- list()
 for(i in 1:ncol(dists)){
   sub <- dists[,i]
   sub <- sub[!is.na(sub)]
   if(length(sub) == 0){
-    nearst_junction[[i]] <- NA
-    distance_junction[[i]] <- NA
+    nearest_junc[[i]] <- NA
+    dist_junc[[i]] <- NA
   } else {
     min_name <- names(sub)[sub == min(sub, na.rm = TRUE)]
-    min_dist <-min(sub, na.rm = TRUE)
-            #find the minimum value and return the name of the minimum value
+    min_dist <- min(sub, na.rm = TRUE)
+            # find the minimum value and return the name of the minimum value
     if(length(min_name) == 0){
       min_name <- NA
       min_dist <- NA
+            # in case that the name of the junction lost
     }
     if(length(min_name) > 1){ 
       min_name <- min_name[1]
       min_dist <- min_dist[1] 
+            # consider the condition that have more than one same min value
     }
-    nearst_junction[[i]] <- min_name
-    distance_junction[[i]] <- min_dist
+    nearest_junc[[i]] <- min_name
+    dist_junc[[i]] <- min_dist
   }
   
 }
-nearst_junction <- unlist(nearst_junction)
-distance_junction <- unlist(distance_junction)
+nearest_junc <- unlist(nearest_junc)
+dist_junc <- unlist(dist_junc)
 
-lines_minor$nearst_junc_dist <- distance_junction
-lines_minor$nearst_junction <- nearst_junction
-lines_minor$major_aadt <- junc_majmi$aadt[match(lines_minor$nearst_junction,
+lines_minor$nearest_junc <- nearest_junc
+lines_minor$nearest_junc_dist <- dist_junc
+lines_minor$major_aadt <- junc_majmi$aadt[match(lines_minor$nearest_junc,
                                                 junc_majmi$from_id)]
+
 # plot each road colored by the AADT on the nearest (in time) major road
 
 lines_minor <- st_transform(lines_minor, 27700)
-qtm(lines_minor, lines.col = "major_aadt", lines.lwd = 2)
+lines_minor_na <- lines_minor[is.na(lines_minor$major_aadt),]
+
+#lines_minor <- lines_minor[!is.na(lines_minor$major_aadt),]
+qtm(bound) + qtm(lines_major,line.col = "ref" ) + 
+  qtm(lines_minor, lines.col = "major_aadt", lines.lwd = 2)
 
 
 st_write(lines_minor, "Data/03_lines_minor.gpkg", delete_dsn = TRUE)
-st_write(minor_cent, "Data/03_minor_cent.gpkg", delete_dsn = TRUE)
+st_write(junc_minor, "Data/03_junc_minor.gpkg", delete_dsn = TRUE)
 st_write(junc_majmi, "Data/03_junction_major_minor.gpkg", delete_dsn = TRUE)
 
 
